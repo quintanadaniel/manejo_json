43c4ac1c89a5b3db409c5d5ff1dcd9c939c0ec2a
Daniel Quintana <quintana.danielarmando@gmail.com>
2019-12-23 09:31:13 -0300

    actualizacion de los cambios 23/12/2019

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
 from elasticsearch import Elasticsearch
 
-from .src.metodos_borrar import borrar_datos_elasticsearch
-from .src.metodos_crear import genera_index
-from .src.metodos_consultar import query_elastic
-from .src.metodos_actualizar import update_json
+#from .src.metodos_borrar import borrar_datos_elasticsearch
+from src.metodos_crear.genera_index import create_index
+from src.archivos_json import doc_json
+#from .src.metodos_consultar import query_elastic
+#from .src.metodos_actualizar import update_json
 
 es = Elasticsearch()
 
-delete_datos = borrar_datos_elasticsearch
-create_datos = genera_index
-consult_datos = query_elastic
-update_datos = update_json
+#delete_datos = borrar_datos_elasticsearch
+#create_datos = genera_index
+#consult_datos = query_elastic
+#update_datos = update_json
 
+def main_principal():
+    print("/---------------------------------------------------------------------------------------------------------------/")
+    print("/---------------- MENU DE OPCIONES PARA CREAR O BORRAR JSON DE ELASTICSEARCH -----------------------------------/")
+    print("/---------------------------------------------------------------------------------------------------------------/")
+    print("/     1. Para crear documentos en los indices                                                                   /")
+    print("/     2. Para borrar documentos de los indices, donde:                                                          /")
+    print("/          a. Borrar Masivamente                                                                                /")
+    print("/          b. Borrar por id                                                                                     /")
+    print("/     3. Para actualizar id documentos de los indices                                                           /")
+    print("/     4. Para consultar  id documentos de los indices                                                           /")
+    print("/---------------------------------------------------------------------------------------------------------------/")
 
-print("---------------------------------------------------------------------------------------------------------------")
-print("---------------- MENU DE OPCIONES PARA CREAR O BORRAR JSON DE ELASTICSEARCH -----------------------------------")
-print("---------------------------------------------------------------------------------------------------------------")
-print("------- 1. Para crear documentos en los indices                                                                ")
-print("------- 2. Para borrar documentos de los indices, donde:                                                       ")
-print("----------- a. Borrar Masivamente                                                                              ")
-print("----------- b. Borrar por id                                                                                   ")
-print("------- 3. Para actualizar id documentos de los indices                                                        ")
-print("------- 4. Para consultar  id documentos de los indices                                                        ")
-print("---------------------------------------------------------------------------------------------------------------")
+    num_opcion = int(input("Ingrese la opcion: "))
 
-num_opcion = int(input("Ingrese la opcion: "))
-
-if num_opcion == 1:
-    pass
-elif num_opcion == 2:
-    tipo_opcion = str(input("Ingrese el tipo de borrado: "))
-    if tipo_opcion == "a":
+    if num_opcion == 1:
         pass
-    elif tipo_opcion == "b":
+    elif num_opcion == 2:
+        tipo_opcion = str(input("Ingrese el tipo de borrado: "))
+        if tipo_opcion == "a":
+            pass
+        elif tipo_opcion == "b":
+            pass
+        else:
+            print("debe ingresar una opcion valida, opcion a o opcion b.")
+    elif num_opcion == 3:
         pass
-    else:
-        print("debe ingresar una opcion valida, opcion a o opcion b.")
-elif num_opcion == 3:
-    pass
-elif num_opcion == 4:
-    pass
+    elif num_opcion == 4:
+        pass
+
+
+if __name__ == '__main__':
+    main_principal()
--- src/metodos_crear/genera_index.py
+++ src/metodos_crear/genera_index.py
@@ -4,34 +4,35 @@ from archivos_json import doc_json
 
 es = Elasticsearch()
 
-print("paso por metodo crear")
-#res = es.index(index="test-index",doc_type='tweet',id=1,body=doc_json.doc0)
-#print(res['result'])
-#res = es.index(index="test-index",doc_type='tweet',id=2,body=doc_json.doc1)
-#print(res['result'])
-#res = es.index(index="test-index",doc_type='tweet',id=3,body=doc_json.doc2)
-#print(res['result'])
-#res = es.index(index="test-index",doc_type='tweet',id=4,body=doc_json.doc3)
-#print(res['result'])
-#res = es.index(index="test-index",doc_type='tweet',id=5,body=doc_json.doc4)
-#print(res['result'])
-#res = es.index(index="test-index",doc_type='tweet',id=6,body=doc_json.doc5)
-#print(res['result'])
-#res = es.index(index="test-index",doc_type='tweet',id=7,body=doc_json.doc6)
-#print(res['result'])
-#res = es.index(index="test-index",doc_type='tweet',id=8,body=doc_json.doc7)
-#print(res['result'])
+def create_index():
+    print("paso por metodo crear")
+    #res = es.index(index="test-index",doc_type='tweet',id=1,body=doc_json.doc0)
+    #print(res['result'])
+    #res = es.index(index="test-index",doc_type='tweet',id=2,body=doc_json.doc1)
+    #print(res['result'])
+    #res = es.index(index="test-index",doc_type='tweet',id=3,body=doc_json.doc2)
+    #print(res['result'])
+    #res = es.index(index="test-index",doc_type='tweet',id=4,body=doc_json.doc3)
+    #print(res['result'])
+    #res = es.index(index="test-index",doc_type='tweet',id=5,body=doc_json.doc4)
+    #print(res['result'])
+    #res = es.index(index="test-index",doc_type='tweet',id=6,body=doc_json.doc5)
+    #print(res['result'])
+    #res = es.index(index="test-index",doc_type='tweet',id=7,body=doc_json.doc6)
+    #print(res['result'])
+    #res = es.index(index="test-index",doc_type='tweet',id=8,body=doc_json.doc7)
+    #print(res['result'])
 
 
-#res = es.get(index="test-index",doc_type='tweet')
-#print(res['_source'])
+    #res = es.get(index="test-index",doc_type='tweet')
+    #print(res['_source'])
 
-#es.indices.refresh(index="test-index")
+    #es.indices.refresh(index="test-index")
 
-# con esto consultamos datos especificos
-#v_autor = "cuadrado"
-#res = es.search(index="test-index", body={"query": {"bool":{ "must": [{ "match": { "autor": v_autor}}]}}})
-#print("Got %d Hits:" % res['hits']['total']['value'])
-#for hit in res['hits']['hits']:
-#    print("%(timestamp)s %(autor)s: %(text)s" % hit["_source"],hit["_id"])
-#    id_es = hit["_id"]
+    # con esto consultamos datos especificos
+    #v_autor = "cuadrado"
+    #res = es.search(index="test-index", body={"query": {"bool":{ "must": [{ "match": { "autor": v_autor}}]}}})
+    #print("Got %d Hits:" % res['hits']['total']['value'])
+    #for hit in res['hits']['hits']:
+    #    print("%(timestamp)s %(autor)s: %(text)s" % hit["_source"],hit["_id"])
+    #    id_es = hit["_id"]
